29a8bcbfd8286924c13b47d41a9eee1655358199
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-03-28 13:43:03 +0200

    Inicjalizacja projektu

---
+++ PUUUU/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PUUUU.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
