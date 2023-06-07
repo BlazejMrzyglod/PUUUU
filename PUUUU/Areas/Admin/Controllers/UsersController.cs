72b22731cc2cab156275f156dada1d9ce3f86ba0
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-06-07 14:50:37 +0200

    Edycja użytkownika
    Do poprawy

--- PUUUU/Areas/Admin/Controllers/UsersController.cs
+++ PUUUU/Areas/Admin/Controllers/UsersController.cs
@@ -9,7 +9,7 @@ namespace PUUUU.Areas.Admin.Controllers
     public class UsersController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
-        public UsersController(UserManager<IdentityUser> userManager) 
+        public UsersController(UserManager<IdentityUser> userManager)
         {
             _userManager = userManager;
         }
@@ -19,51 +19,38 @@ namespace PUUUU.Areas.Admin.Controllers
             return View(_userManager.Users);
         }
 
-        // GET: UsersController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: UsersController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: UsersController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
         // GET: UsersController/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(string id)
         {
-            return View();
+            return View(_userManager.FindByIdAsync(id).Result);
         }
 
         // POST: UsersController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(string id, [Bind("UserName, Email, EmailConfirmed, PhoneNumber, PhoneNumberConfirmed, AccessFailedCount, LockoutEnabled, LockoutEnd, TwoFactorEnabled")] IdentityUser updatedUser, string role)
         {
             try
             {
+                var oldUser = _userManager.FindByIdAsync(id).Result;
+                oldUser.UserName = updatedUser.UserName;
+                oldUser.Email = updatedUser.Email;
+                oldUser.EmailConfirmed = updatedUser.EmailConfirmed;
+                oldUser.PhoneNumber = updatedUser.PhoneNumber;
+                oldUser.PhoneNumberConfirmed = updatedUser.PhoneNumberConfirmed;
+                oldUser.AccessFailedCount = updatedUser.AccessFailedCount;
+                oldUser.LockoutEnabled = updatedUser.LockoutEnabled;
+                oldUser.LockoutEnd = updatedUser.LockoutEnd;
+                oldUser.TwoFactorEnabled = updatedUser.TwoFactorEnabled;
+                _userManager.UpdateAsync(oldUser);
+                var oldRole = _userManager.GetRolesAsync(oldUser).Result;
+                _userManager.RemoveFromRolesAsync(oldUser, oldRole);
+                _userManager.AddToRoleAsync(oldUser, role);
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                return View(updatedUser);
             }
         }
 
