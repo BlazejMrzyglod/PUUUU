ada5c914c5332abd6ef2c38650c6ab1bb9d755e9
Jakub Pszczółka <jp057198@student.ath.edu.pl>
2023-06-12 00:45:32 +0200

    Wszystkie podstrony dla Firmy w stopce

--- PUUUU/Controllers/HomeController.cs
+++ PUUUU/Controllers/HomeController.cs
@@ -32,6 +32,16 @@ namespace PUUUU.Controllers
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        
+        public IActionResult Service()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ PUUUU/Views/Home/Service.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PUUUU.Views.Home
+{
+    public class ServiceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
