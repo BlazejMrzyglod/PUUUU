298f9bcdd2ad6f6f9178709808bb3b4954ac00e7
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-06-12 17:50:57 +0200

    Naprawa wstawiania danych

--- PUUUU/Program.cs
+++ PUUUU/Program.cs
@@ -57,30 +57,30 @@ using (var scope = app.Services.CreateScope())
 
     List<Bike> bikes = new List<Bike>()
         {
-            new Bike() { Id = 1, Brand = "Trek", Model = "Fuel EX 8", Type = "Mountain Bike", Size = "Medium", Price = 3500.00, Quantity = 3, Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk=" },
-            new Bike() { Id = 2, Brand = "Specialized", Model = "Diverge", Type = "Gravel Bike", Size = "Large", Price = 2500.00, Quantity = 5, Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk="},
-            new Bike() { Id = 3, Brand = "Giant", Model = "TCR Advanced Pro 2", Type = "Road Bike", Size = "Small", Price = 3600.00, Quantity = 2 , Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk="},
-            new Bike() { Id = 4, Brand = "Cannondale", Model = "Topstone Carbon", Type = "Gravel Bike", Size = "Medium", Price = 3200.00, Quantity = 4, Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk=" },
-            new Bike() { Id = 5, Brand = "Santa Cruz", Model = "Hightower C S", Type = "Mountain Bike", Size = "Large", Price = 5400.00, Quantity = 1, Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk=" },
-            new Bike() { Id = 6, Brand = "Kona", Model = "Sutra LTD", Type = "Touring Bike", Size = "Small", Price = 2700.00, Quantity = 2, Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk=" },
-            new Bike() { Id = 7, Brand = "Scott", Model = "Addict RC 20", Type = "Road Bike", Size = "Medium", Price = 3800.00, Quantity = 1, Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk=" },
-            new Bike() { Id = 8, Brand = "Surly", Model = "Bridge Club", Type = "Gravel Bike", Size = "Large", Price = 1800.00, Quantity = 6, Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk=" },
-            new Bike() { Id = 9, Brand = "Cervelo", Model = "Aspero-5 GRX 810", Type = "Gravel Bike", Size = "Medium", Price = 4900.00, Quantity = 3, Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk=" },
-            new Bike() { Id = 10, Brand = "Bianchi", Model = "Infinito CV Disc", Type = "Road Bike", Size = "Large", Price = 4600.00, Quantity = 2, Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk=" },
+            new Bike() {Brand = "Trek", Model = "Fuel EX 8", Type = "Mountain Bike", Size = "Medium", Price = 3500.00, Quantity = 3, Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk=" },
+            new Bike() { Brand = "Specialized", Model = "Diverge", Type = "Gravel Bike", Size = "Large", Price = 2500.00, Quantity = 5, Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk="},
+            new Bike() { Brand = "Giant", Model = "TCR Advanced Pro 2", Type = "Road Bike", Size = "Small", Price = 3600.00, Quantity = 2 , Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk="},
+            new Bike() { Brand = "Cannondale", Model = "Topstone Carbon", Type = "Gravel Bike", Size = "Medium", Price = 3200.00, Quantity = 4, Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk=" },
+            new Bike() { Brand = "Santa Cruz", Model = "Hightower C S", Type = "Mountain Bike", Size = "Large", Price = 5400.00, Quantity = 1, Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk=" },
+            new Bike() { Brand = "Kona", Model = "Sutra LTD", Type = "Touring Bike", Size = "Small", Price = 2700.00, Quantity = 2, Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk=" },
+            new Bike() { Brand = "Scott", Model = "Addict RC 20", Type = "Road Bike", Size = "Medium", Price = 3800.00, Quantity = 1, Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk=" },
+            new Bike() { Brand = "Surly", Model = "Bridge Club", Type = "Gravel Bike", Size = "Large", Price = 1800.00, Quantity = 6, Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk=" },
+            new Bike() { Brand = "Cervelo", Model = "Aspero-5 GRX 810", Type = "Gravel Bike", Size = "Medium", Price = 4900.00, Quantity = 3, Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk=" },
+            new Bike() { Brand = "Bianchi", Model = "Infinito CV Disc", Type = "Road Bike", Size = "Large", Price = 4600.00, Quantity = 2, Image = "https://media.istockphoto.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk=" },
         };
 
     List<BikePart> bikeParts = new List<BikePart>
         {
-            new BikePart { Id = 1, Name = "Shimano Ultegra Crankset", Description = "High-performance crankset for road bikes", Type = "Frame", Price = 349.99, Quantity = 10 },
-            new BikePart { Id = 2, Name = "SRAM GX Eagle Cassette", Description = "12-speed cassette for mountain bikes", Type = "Fork", Price = 149.99, Quantity = 5 },
-            new BikePart { Id = 3, Name = "Continental Grand Prix 5000 Tires", Description = "Top-of-the-line road bike tires with excellent grip and durability", Type = "Wheels", Price = 79.99, Quantity = 20 },
-            new BikePart { Id = 4, Name = "RockShox Reverb Dropper Post", Description = "Adjustable dropper post for mountain bikes", Type = "Saddle", Price = 399.99, Quantity = 8 },
-            new BikePart { Id = 5, Name = "Shimano XT Disc Brake Set", Description = "Hydraulic disc brake set for mountain bikes", Type = "Handle", Price = 249.99, Quantity = 12 },
-            new BikePart { Id = 6, Name = "Schwalbe Marathon Plus Tires", Description = "Highly puncture-resistant touring bike tires", Type = "Pedals", Price = 59.99, Quantity = 15 },
-            new BikePart { Id = 7, Name = "Shimano Deore XT Rear Derailleur", Description = "10-speed rear derailleur for mountain bikes", Type = "Frame", Price = 89.99, Quantity = 6 },
-            new BikePart { Id = 8, Name = "Brooks B17 Saddle", Description = "Classic leather saddle for touring bikes", Type = "Saddle", Price = 129.99, Quantity = 3 },
-            new BikePart { Id = 9, Name = "Park Tool PCS-10.2 Bike Repair Stand", Description = "Portable bike repair stand for home mechanics", Type = "Wheels", Price = 219.99, Quantity = 4 },
-            new BikePart { Id = 10, Name = "Thomson Elite Seatpost", Description = "High-strength aluminum seatpost for road and mountain bikes", Type = "Handle", Price = 79.99,Quantity = 7 },
+            new BikePart {  Name = "Shimano Ultegra Crankset", Description = "High-performance crankset for road bikes", Type = "Frame", Price = 349.99, Quantity = 10 },
+            new BikePart { Name = "SRAM GX Eagle Cassette", Description = "12-speed cassette for mountain bikes", Type = "Fork", Price = 149.99, Quantity = 5 },
+            new BikePart { Name = "Continental Grand Prix 5000 Tires", Description = "Top-of-the-line road bike tires with excellent grip and durability", Type = "Wheels", Price = 79.99, Quantity = 20 },
+            new BikePart { Name = "RockShox Reverb Dropper Post", Description = "Adjustable dropper post for mountain bikes", Type = "Saddle", Price = 399.99, Quantity = 8 },
+            new BikePart { Name = "Shimano XT Disc Brake Set", Description = "Hydraulic disc brake set for mountain bikes", Type = "Handle", Price = 249.99, Quantity = 12 },
+            new BikePart { Name = "Schwalbe Marathon Plus Tires", Description = "Highly puncture-resistant touring bike tires", Type = "Pedals", Price = 59.99, Quantity = 15 },
+            new BikePart { Name = "Shimano Deore XT Rear Derailleur", Description = "10-speed rear derailleur for mountain bikes", Type = "Frame", Price = 89.99, Quantity = 6 },
+            new BikePart { Name = "Brooks B17 Saddle", Description = "Classic leather saddle for touring bikes", Type = "Saddle", Price = 129.99, Quantity = 3 },
+            new BikePart { Name = "Park Tool PCS-10.2 Bike Repair Stand", Description = "Portable bike repair stand for home mechanics", Type = "Wheels", Price = 219.99, Quantity = 4 },
+            new BikePart { Name = "Thomson Elite Seatpost", Description = "High-strength aluminum seatpost for road and mountain bikes", Type = "Handle", Price = 79.99,Quantity = 7 },
         };
     bikes.ForEach(b => db.Bikes.Add(b));
     bikeParts.ForEach(b => db.BikeParts.Add(b));
@@ -93,24 +93,21 @@ using (var scope = app.Services.CreateScope())
     if (roleManager != null)
     {
         if (!roleManager.RoleExistsAsync("Administrator").Result)
-            roleManager.CreateAsync(new IdentityRole()
+            await roleManager.CreateAsync(new IdentityRole()
             {
                 Name = "Administrator"
             });
         if (!roleManager.RoleExistsAsync("User").Result)
-            roleManager.CreateAsync(new IdentityRole()
-            {
-                Name = "User"
-            });
+            await roleManager.CreateAsync(new IdentityRole() { Name = "User" });
     }
 
-    var createAdmin = userManager.CreateAsync(
+    var createAdmin = await userManager.CreateAsync(
         new IdentityUser()
         {
             UserName = "test@test.com",
             Email = "test@test.com",
         }, "Test12345!");
-    var admin = userManager.FindByNameAsync("test@test.com").Result;
+    var admin =  userManager.FindByNameAsync("test@test.com").Result;
 
     var verificationCode = userManager.GenerateEmailConfirmationTokenAsync(admin).Result;
     // code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
