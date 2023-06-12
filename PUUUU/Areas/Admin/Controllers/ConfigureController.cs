389017ac733c683ebb6e48a235003987b4838d9e
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-06-12 15:38:04 +0200

    Przeniesienie konfiguratora do własnego kontrolera

---
+++ PUUUU/Areas/Admin/Controllers/ConfigureController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PUUUU.Areas.Admin.Controllers
+{
+    public class ConfigureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
