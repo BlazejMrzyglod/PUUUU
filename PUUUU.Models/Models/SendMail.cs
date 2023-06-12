1ea02d1f3019ce75ccd4bd63ba364a6138c8f51c
Jakub Pszczółka <jp057198@student.ath.edu.pl>
2023-06-12 14:31:32 +0200

    Modele Kontaktowe i walidacja

---
+++ PUUUU.Models/Models/SendMail.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUUUU.Models.Models
+{
+    public class SendMail : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            using (MailMessage mailMessage = new MailMessage()) 
+            {
+                mailMessage.From = new MailAddress("example@email.com");
+                mailMessage.Subject = subject;
+                mailMessage.Body = email + htmlMessage;
+                mailMessage.IsBodyHtml = true;
+                mailMessage.To.Add(new MailAddress("example@email.com"));
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.EnableSsl = true;
+                System.Net.NetworkCredential NetworkCred = new System.Net.NetworkCredential();
+                NetworkCred.UserName = "";
+                NetworkCred.Password = "";
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = NetworkCred;
+                smtp.Port = 25;
+               await smtp.SendMailAsync(mailMessage);
+            }
+        }
+    }
+}
---
+++ PUUUU.Models/ViewModels/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUUUU.Models.ViewModels
+{
+    public class Contact
+    {
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        public string? Email { get; set; }
+        [Display(Name = "Imię")]
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        public string? Name { get; set; }
+        [Display(Name = "Wiadomość")]
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        public string? Message { get; set; }
+    }
+}
