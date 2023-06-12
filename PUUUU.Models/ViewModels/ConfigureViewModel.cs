a5d2b63ce7a3fc69bdd5edbd1aae44cfdbea886e
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-06-12 15:36:18 +0200

    Dodan ViewModel konfiguratora

---
+++ PUUUU.Models/ViewModels/ConfigureViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUUUU.Models.ViewModels
+{
+    public class ConfigureViewModel
+    {
+        public SelectList Frames { get; set; }
+        public SelectList Forks { get; set; }
+        public SelectList Wheels { get; set; }
+        public SelectList Saddles { get; set; }
+        public SelectList Handles { get; set; }
+        public SelectList Pedals { get; set; }
+    }
+}
