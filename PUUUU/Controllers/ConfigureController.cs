bfda1f965ada278046eceb0fb64e2c0b5a8282dd
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-06-12 17:50:39 +0200

    Złożenie pustego zamówienia po wybraniu części

--- PUUUU/Controllers/ConfigureController.cs
+++ PUUUU/Controllers/ConfigureController.cs
@@ -1,18 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using PUUUU.Data;
 using PUUUU.Models.Models;
 using PUUUU.Models.ViewModels;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PUUUU.Controllers
 {
     public class ConfigureController : Controller
     {
         private readonly ApplicationDbContext _context;
-
-        public ConfigureController(ApplicationDbContext context)
+        private readonly UserManager<IdentityUser> _userManager;
+        public ConfigureController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
@@ -25,6 +28,26 @@ namespace PUUUU.Controllers
             viewModel.Handles = new SelectList(parts.Where(e => e.Type == "Handle"), "Id", "Name", viewModel.HandleId);
             viewModel.Pedals = new SelectList(parts.Where(e => e.Type == "Pedals"), "Id", "Name", viewModel.PedalsId);
             return View(viewModel);
+        }
+        [HttpPost]
+        public IActionResult Index([Bind("FrameId,ForkId,WheelsId,SaddleId,HandleId,PedalsId")] ConfigureViewModel configuration)
+        {
+            ConfigureOrder order = new ConfigureOrder();
+            order.Address = " ";
+            order.DeliveryMethod = " ";
+            order.PaymentMethod = " ";
+            order.CreatedDate = DateTime.Now;
+            order.User = _userManager.FindByNameAsync("test@test.com").Result;
+            order.Parts = new List<BikePart>();
+            order.Parts.Add(_context.BikeParts.Find(configuration.FrameId));
+            order.Parts.Add(_context.BikeParts.Find(configuration.ForkId));
+            order.Parts.Add(_context.BikeParts.Find(configuration.WheelsId));
+            order.Parts.Add(_context.BikeParts.Find(configuration.SaddleId));
+            order.Parts.Add(_context.BikeParts.Find(configuration.HandleId));
+            order.Parts.Add(_context.BikeParts.Find(configuration.PedalsId));
+            _context.ConfigureOrders.Add(order);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
