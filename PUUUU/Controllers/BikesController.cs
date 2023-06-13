e32ea5d5048a08effbb35bbdb3e6125c1fd689fb
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-06-14 01:27:09 +0200

    Dodanie możliwości składania zamówienia na rower

--- PUUUU/Controllers/BikesController.cs
+++ PUUUU/Controllers/BikesController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -13,18 +15,20 @@ namespace PUUUU.Controllers
     public class BikesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public BikesController(ApplicationDbContext context)
+        public BikesController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // GET: Bikes
         public async Task<IActionResult> Index()
         {
-              return _context.Bikes != null ? 
-                          View(await _context.Bikes.ToListAsync()) :
-                          Problem("Entity set 'ApplicationDbContext.Bikes'  is null.");
+            return _context.Bikes != null ?
+                        View(await _context.Bikes.ToListAsync()) :
+                        Problem("Entity set 'ApplicationDbContext.Bikes'  is null.");
         }
 
         // GET: Bikes/Details/5
@@ -150,14 +154,39 @@ namespace PUUUU.Controllers
             {
                 _context.Bikes.Remove(bike);
             }
-            
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Bikes/Order
+        [Authorize]
+        public IActionResult Order(int id)
+        {
+            return View();
+        }
+
+        // POST: Bikes/Order
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Order(int id, [Bind("Address,PaymentMethod,DeliveryMethod")] BikeOrder order, string userName)
+        {
+            var user = _userManager.FindByNameAsync(userName).Result;
+            order.User = user;
+
+            order.CreatedDate = DateTime.Now;
+            order.BikeId = id;
+            _context.Add(order);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool BikeExists(int id)
         {
-          return (_context.Bikes?.Any(e => e.Id == id)).GetValueOrDefault();
+            return (_context.Bikes?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
