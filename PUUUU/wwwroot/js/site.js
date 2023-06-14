5b674e52b88683ea532f1c83af986df42ead86ec
Radek337 <radek.koczur2@gmail.com>
2023-06-14 17:02:33 +0200

    simple js runtime tests

--- PUUUU/wwwroot/js/site.js
+++ PUUUU/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+(
+    () => {
+        console.log("Pliki js wgrane poprawnie!")
+        let braki = document.querySelectorAll("td")
+        let errors = 0
+        braki.forEach((x) => {
+            if (x.innerHTML == 0) {
+                console.log("Błedy w wypełnieniu")
+                errors++;
+            }
+        })
+        console.log("błędy:"+errors)
+    })();
