1707aad7deb0bf1db3e2b0963dc7fe107671767f
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2023-06-14 15:27:07 +0200

    Przeniesienie rzeczy które użytkownik nie powinien widzieć do Admina

---
+++ PUUUU/Areas/Admin/Controllers/BikesController.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PUUUU.Data;
+using PUUUU.Models.Models;
+
+namespace PUUUU.Controllers
+{
+    [Area("Admin")]
+    public class BikesAdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public BikesAdminController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Bikes
+        public async Task<IActionResult> Index()
+        {
+            return _context.Bikes != null ?
+                        View(await _context.Bikes.ToListAsync()) :
+                        Problem("Entity set 'ApplicationDbContext.Bikes'  is null.");
+        }
+
+        // GET: Bikes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Bikes == null)
+            {
+                return NotFound();
+            }
+
+            var bike = await _context.Bikes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bike == null)
+            {
+                return NotFound();
+            }
+
+            return View(bike);
+        }
+
+        // GET: Bikes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bikes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Brand,Model,Type,Size,Price,Quantity")] Bike bike)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bike);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bike);
+        }
+
+        // GET: Bikes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Bikes == null)
+            {
+                return NotFound();
+            }
+
+            var bike = await _context.Bikes.FindAsync(id);
+            if (bike == null)
+            {
+                return NotFound();
+            }
+            return View(bike);
+        }
+
+        // POST: Bikes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Brand,Model,Type,Size,Price,Quantity")] Bike bike)
+        {
+            if (id != bike.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bike);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BikeExists(bike.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bike);
+        }
+
+        // GET: Bikes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Bikes == null)
+            {
+                return NotFound();
+            }
+
+            var bike = await _context.Bikes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bike == null)
+            {
+                return NotFound();
+            }
+
+            return View(bike);
+        }
+
+        // POST: Bikes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Bikes == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Bikes'  is null.");
+            }
+            var bike = await _context.Bikes.FindAsync(id);
+            if (bike != null)
+            {
+                _context.Bikes.Remove(bike);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Bikes/Order
+        [Authorize]
+        public IActionResult Order(int id)
+        {
+            return View();
+        }
+
+        // POST: Bikes/Order
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Order(int id, [Bind("Address,PaymentMethod,DeliveryMethod")] BikeOrder order, string userName)
+        {
+            var user = _userManager.FindByNameAsync(userName).Result;
+            order.User = user;
+
+            order.CreatedDate = DateTime.Now;
+            order.BikeId = id;
+            _context.Add(order);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BikeExists(int id)
+        {
+            return (_context.Bikes?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
